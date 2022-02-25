f6e6a3cb22e7612cb1f308b8d7619fbd402e97e2
cmwolfe <cmwolfe.dev@gmail.com>
2022-02-25 14:23:34 -0600

    Added get_total_amount_in_inputs and outputs, updated validate funds to have just the assert.get_total_amount in inputs == outputs line

--- src/node/node.py
+++ src/node/node.py
@@ -4,6 +4,7 @@ from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto.Signature import pkcs1_15
 
+from utils import calculate_hash
 from node.block import Block
 from transaction_inputs import TransactionInput
 from transaction_outputs import TransactionOutput
@@ -15,6 +16,7 @@ class NodeTransaction:
         self.transaction_data = ""
         self.signature = ""
 
+    # This will validate each of the transaction input's signatures
     def validate_signature(self):
         transaction_data = copy.deepcopy(self.transaction_data)
         for count, tx_input in enumerate(transaction_data["inputs"]):
@@ -28,19 +30,59 @@ class NodeTransaction:
             transaction_bytes = json.dumps(transaction_data, indent=2).encode('utf-8')
             transaction_hash = SHA256.new(transaction_bytes)
 
+    # This adds up all the totals from the referenced UTXO's and
+    # validates that they match the amounts in the transaction output
     def validate_funds(self, sender_address: bytes, amount: int) -> bool:
-        sender_balance = 0
+        assert self.get_total_amount_in_inputs() == self.get_total_amount_in_outputs()
+
+        # sender_balance = 0
+        # current_block = self.blockchain
+        # while current_block:
+        #     if current_block.transaction_data["sender"] == sender_address:
+        #         sender_balance = sender_balance - current_block.transaction_data["amount"]
+        #     if current_block.transaction_data["receiver"] == sender_address:
+        #         sender_balance = sender_balance + current_block.transaction_data["amount"]
+        #     current_block = current_block.previous_block
+        # if amount <= sender_balance:
+        #     return True
+        # else:
+        #    return False
+
+
+    def get_total_amount_in_inputs(self) -> int:
+        total_in = 0
+        for tx_input in self.inputs:
+            input_dict = json.loads(tx_input)
+            transaction_data = self.get_transaction_from_utxo(input_dict["transaction_hash"])
+            utxo_amount = json.loads(transaction_data["outputs"][input_dict["output_index"]])["amount"]
+            total_in  = total_in + utxo_amount
+        return total_in
+
+    def get_total_amount_in_outputs(self) -> int:
+        total_out = 0
+        for tx_output in self.outputs:
+            output_dict = json.loads(tx_output)
+            amount = output_dict["amount"]
+            total_out = total_out + amount
+        return total_out
+
+    def get_transaction_from_utxo(self, utxo_hash: str) -> dict:
         current_block = self.blockchain
         while current_block:
-            if current_block.transaction_data["sender"] == sender_address:
-                sender_balance = sender_balance - current_block.transaction_data["amount"]
-            if current_block.transaction_data["receiver"] == sender_address:
-                sender_balance = sender_balance + current_block.transaction_data["amount"]
+            if utxo_hash == current_block.transaction_hash:
+                return current_block.transaction_data
             current_block = current_block.previous_block
-        if amount <= sender_balance:
-            return True
-        else:
-            return False
+
+    # This will validate that each of the UTXO's receiver address
+    # match the sender's public key
+    def confirm_sender_owns_funds(self):
+        for tx_input in self.inputs:
+            input_dict = json.loads(tx_input)
+            public_key = input_dict["public_key"]
+            sender_public_key_hash = calculate_hash(calculate_hash(public_key, hash_function="sha256"), hash_function="ripemd160")
+            transaction_data = self.get_transaction_from_utxo(input_dict["transaction_hash"])
+            public_key_hash = json.loads(transaction_data["outputs"][input_dict["output_index"]])["public_key_hash"]
+            assert public_key_hash == sender_public_key_hash
 
     def validate(self):
         self.validate_signature()
